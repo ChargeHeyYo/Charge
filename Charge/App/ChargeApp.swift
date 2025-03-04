c85eeae6b59e67e559232977a85ffee1f7730d02
tenedict <tenedict@gmail.com>
2025-03-04 14:25:07 +0900

    [Feat] Charge플젝생성

---
+++ Charge/App/ChargeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeApp.swift
+//  Charge
+//
+//  Created by 문재윤 on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChargeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charge/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Charge
+//
+//  Created by 문재윤 on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
